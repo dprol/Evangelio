e20948afb6845eb9aaad4d37012f31fc4cec83ca
Marc Nafria <marc.nafria@gmail.com>
2024-10-16 18:06:18 +0200

    wksadjfjwsd

--- scripts/navegacio.js
+++ scripts/navegacio.js
@@ -141,4 +141,13 @@ export function inici_pagines_amb_animacio() {
                 break;
         }
     });
+
+    pages.forEach((page, index) => {
+        page.addEventListener('scroll', () => {
+            // Check if the scroll position is within 40px of the top
+            if (page.scrollTop <= 40) {
+                page.scrollTop = 0; // Snap to the top
+            }
+        });
+    });
 }
